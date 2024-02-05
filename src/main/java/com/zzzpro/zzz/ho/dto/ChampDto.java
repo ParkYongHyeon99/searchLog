fe6c552dad735147ca59177190c80cdcb49a3dc6
song5 <song5@BOOK-2QDPVMDSOF>
2024-02-05 16:53:02 +0900

    0205 용호

---
+++ src/main/java/com/zzzpro/zzz/ho/dto/ChampDto.java
@@ -0,0 +1,21 @@
+package com.zzzpro.zzz.ho.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ChampDto {
+	private String matchid;
+	private int gameduration;
+	private String riotidgamename;
+	private String riotidtagline;
+	private int participantid;
+	private String championName;
+	private String teamPosition;
+	private String win;
+	private String win_percentage;
+
+}
