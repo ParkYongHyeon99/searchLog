83b7d490ed45f0559e481645da6713e6bb521ec4
10sol21 <00thf@naver.com>
2024-02-10 16:33:26 +0900

    0210 sol test

--- src/main/java/com/zzzpro/zzz/sol/dto/LolmateDto.java
+++ src/main/java/com/zzzpro/zzz/sol/dto/LolmateDto.java
@@ -8,14 +8,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class LolmateDto {
-	private String gameMate;	// 게임메이트 (듀오/멘토)
-	private String gameMode;	// 게임모드 (솔로랭크/뭐더라아무튼이런거***)
-	private String user;	// 닉네임#태그
-	private String tier;	// 티어 (위 닉네임#태그를 통해 검색해온 티어를 저장할 예정)
-	private String myPosition;	// 글 올린 사람의 주 포지션
-	private String yourPosition;	// 글 올린 사람이 찾는 포지션
-	private double winrate;	// 글 올린 사람의 승률 (글 올릴 때의 승률로 고정 저장하거나, 리스트를 불러올때 검색하기?< 오래 걸릴듯)
-	private double duoWinrate;	// 글 올린 사람과 신청자의 듀오 시 예상 승률 확인 (할 수 없을 것 같긴 한데 ㅠㅠ)
-	private String memo;	// 글 올린 사람의 메모(할 말)
-	private int discord;	// 디스코드(통화) 여부
+	private int lm_num;	// 글 번호
+	private String lm_gameMate;	// 게임메이트 (듀오/멘토)
+	private String lm_gameMode;	// 게임모드 (솔로랭크/뭐더라아무튼이런거***)
+	private String m_nick;	// 작성자
+	private String lm_summonerName;	// 작성자 닉네임#태그
+	private String lm_tier;	// 티어 (위 닉네임#태그를 통해 검색해온 티어를 저장할 예정)
+	private String lm_myPosition;	// 글 올린 사람의 주 포지션
+	private String lm_findPosition;	// 글 올린 사람이 찾는 포지션
+	private String lm_memo;	// 글 올린 사람의 메모(할 말)
+	private int lm_discord;	// 디스코드(통화) 여부
+	
+	/* DB 저장 X */
+	private double winrate;	// 글 올린 사람의 승률 (리스트를 불러올때 검색)
+	private double duoWinrate;	// 글 올린 사람과 신청자의 듀오 시 예상 승률 확인
 }
