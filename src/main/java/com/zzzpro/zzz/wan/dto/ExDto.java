14a43a0e1b84e329f0d366a3098c48aeebf4d2f0
backendmoon <acms562798@gmail.com>
2024-02-16 17:50:20 +0900

    0216 지완 - 챔피언 분석페이지 초상화 및 상세페이지 추가

---
+++ src/main/java/com/zzzpro/zzz/wan/dto/ExDto.java
@@ -0,0 +1,41 @@
+package com.zzzpro.zzz.wan.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExDto {
+	private String champions; 
+	private String champion_name; 
+	private String championName;
+	private String teamPosition;
+	private String linePick;
+	
+	private String matchid;
+	private int gameduration;
+	private String riotidgamename;
+	private String riotidtagline;
+	private int participantid;
+	private String win;
+	private String win_percentage;
+	private String pick_percentage;
+	private String skill_tree;
+	private String tier1;
+	private String championName_KR;
+	private String Count;
+	private String percentage;
+	private String item0;
+	private String item1;
+	private String item2;
+	private String item3;
+	private String item4;
+	private String item5;
+	private String item6;
+	private String shoes;
+	private String core1;
+	private String core2;
+	private String core3;
+}
