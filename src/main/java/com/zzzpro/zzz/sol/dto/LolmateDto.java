a9546204edabab1bb1bc0ae1e499645552b83368
10sol21 <00thf@naver.com>
2024-02-21 16:51:56 +0900

    0221 한솔 롤메이트 페이지 작성 시 찾는 포지션 선택 추가 작업

--- src/main/java/com/zzzpro/zzz/sol/dto/LolmateDto.java
+++ src/main/java/com/zzzpro/zzz/sol/dto/LolmateDto.java
@@ -13,14 +13,14 @@ public class LolmateDto {
 	private String lm_gameMode;	// 게임모드 (솔로랭크 * * *)
 	private String m_nick;	// 작성자
 	private String lm_summonerName;	// 작성자 닉네임#태그
-	private String lm_tier;	// 티어 (위 닉네임#태그를 통해 검색해온 티어를 저장할 예정)
 	private String lm_myPosition;	// 글 올린 사람의 주 포지션
 	private String lm_findPosition;	// 글 올린 사람이 찾는 포지션
 	private String lm_memo;	// 글 올린 사람의 메모(할 말)
 	private int lm_discord;	// 디스코드(통화) 여부
-	private int lm_end;
+	private int lm_end;	// 듀오/멘토 신청 가능 여부
 	
 	/* DB 저장 X */
+	private String lm_tier;	// 티어 (위 닉네임#태그를 통해 검색해온 티어를 저장할 예정)
 	private double winrate;	// 글 올린 사람의 승률 (리스트를 불러올때 검색)
 	private double duoWinrate;	// 글 올린 사람과 신청자의 듀오 시 예상 승률 확인
 }
