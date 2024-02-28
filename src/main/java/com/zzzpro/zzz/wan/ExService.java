7047e3695d23cebb0ba2682999e65c343d2b637f
backendmoon <acms562798@gmail.com>
2024-02-28 19:06:44 +0900

    11

--- src/main/java/com/zzzpro/zzz/wan/ExService.java
+++ src/main/java/com/zzzpro/zzz/wan/ExService.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
