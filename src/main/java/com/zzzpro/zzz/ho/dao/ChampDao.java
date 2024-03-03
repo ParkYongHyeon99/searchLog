a48898db82d91eea5530fd8d745f0ab1f7307462
song5 <song5@BOOK-2QDPVMDSOF>
2024-03-04 08:51:26 +0900

    0304 용호 챔피언 분석 쿼리 일부 추가, css 포함

--- src/main/java/com/zzzpro/zzz/ho/dao/ChampDao.java
+++ src/main/java/com/zzzpro/zzz/ho/dao/ChampDao.java
@@ -1,19 +1,34 @@
 package com.zzzpro.zzz.ho.dao;
 
-import java.util.ArrayList;
+
+import java.util.HashMap;
+import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import com.zzzpro.zzz.ho.dto.ChampDto;
 
+import jakarta.servlet.http.HttpSession;
+
 @Mapper
 public interface ChampDao {
+	@Select("select championName, championName_kr from champList1 order by championName_kr")
+	List<ChampDto> champions(String champoins);
+	List<ChampDto> champpick_pi(ChampDto cDto);
 	String winrate(ChampDto champDto);
-	String pickrate(ChampDto champDto);
+	String pickrate(ChampDto cDto);
 	ChampDto banrate(ChampDto champDto);
-	ChampDto skill_tree_winrate(ChampDto champDto);
-	ChampDto item_winrate(ChampDto champDto);
-	ChampDto shoes_winrate(ChampDto champDto);
-	ChampDto Rune_winrate(ChampDto champDto);
-
+	
+	List<ChampDto> item_winrate(ChampDto champDto);
+	List<ChampDto> shoes_winrate(ChampDto champDto);
+	List<ChampDto> Rune_winrate(ChampDto champDto);
+	List<ChampDto> stat_rate(ChampDto champDto);
+	List<ChampDto> spell_rate(ChampDto champDto);
+	List<ChampDto> acc_winrate(ChampDto champDto);
+	List<ChampDto> start_item(ChampDto champDto);
+	List<ChampDto> core1(ChampDto champDto);
+	List<ChampDto> core2(ChampDto champDto);
+	List<ChampDto> skill_tree_3lv(ChampDto champDto);
+	
 }
